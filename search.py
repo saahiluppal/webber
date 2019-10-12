ed8095ffc8f4c86768fd1b3cc1aaaa2f2048d65a
Sahil Uppal <sahil2kuppal@gmail.com>
2019-10-12 17:07:23 +0530

    compiled upto entity

--- search.py
+++ search.py
@@ -1,64 +1,115 @@
-from entity import Entity
-from web_results import Web_results
-import webbrowser
-import os
-import time
-from color import Color
+import requests
+from bs4 import BeautifulSoup
 
-color = Color()
+query = input("Query:: ")
+parser = 'lxml'
 
-def entity_function(query):
-    os.system('clear')
-    entity_res = Entity(query)
-    if not entity_res.get_title():
-        return False
-    title = entity_res.get_title()[0] 
-    typee = entity_res.get_type()[0] if entity_res.get_type() else 'Unknown'
-    url = entity_res.get_url()[0] if entity_res.get_url() else 'Unknown'
-    description = entity_res.get_description()[0] if entity_res.get_description() else 'Unknown'
-    wikipedialink = entity_res.get_wikipedia_link()[0] if entity_res.get_wikipedia_link() else 'Unknown'
-    textunder = entity_res.get_wikipedia_link()[1] if entity_res.get_wikipedia_link() else 'Unknown'
-    if entity_res.get_spell_title():
-        return [color.RED+entity_res.get_spell_title()+color.END,color.RED+entity_res.get_spell_description()+color.END,'\n',
-            "Title :: "+title,"Type :: "+typee,url,color.YELLOW+description+color.END,
-        color.BLUE+"Wikipedia :: "+wikipedialink+color.END,color.BLUE+"Text Under :: "+textunder+color.END]
-    else:
-        return ["Title :: "+title,"Type :: "+typee,url,color.YELLOW+description+color.END,
-        color.BLUE+"Wikipedia :: "+wikipedialink+color.END,color.BLUE+"Text Under :: "+textunder+color.END]
+## First part Loading the page
+response = requests.get('https://www.entireweb.com/web?q='+query)
 
-def web_function(query):
-    web_res = Web_results(query)
-    if not web_res.get_site_title():
-        webbrowser.open('https://www.entireweb.com/web?q='+query)
-        return False
-    print_web(web_res)
+# Finding Soup and decomposing advertisements
+content = response.content
+soup = BeautifulSoup(content,parser)
+advertisements = soup.find_all('div',{'class':'web-results lastad'})
+for i in advertisements:
+    i.decompose()
 
-def print_web(web_res):
-    title = web_res.get_site_title()
-    url = web_res.get_site_url()
-    description = web_res.get_site_description()
+# Getting Spell title
+if soup.find('div',{'class':'web-results','class':'query-spell'}) == None:
+    spell_title = False
+else:
+    spell_title = soup.find('div',{'class':'web-results','class':'query-spell','class':'spell-title'}).text
+    try: 
+        spell_title = spell_title.replace('\n','')
+    except:
+        pass
+    spell_title = spell_title.strip()
+print(spell_title)
 
-    for i,j,k in zip(title,url,description):
-        print(color.BOLD+i+color.END)
-        print(j)
-        print(k)
-        print()
+# Getting spell description
+if soup.find('div',{'class':'web-results','class':'query-spell'}) == None:
+    spell_description = False
+else:
+    spell_description = soup.find('div',{'class':'web-results','class':'query-spell','class':'spell-description'}).text
+    try:  
+        spell_description = spell_description.replace('\n','')
+    except:
+        pass
+    spell_description = spell_description.strip()
+print(spell_description)
 
+## Second part loading the entity
+# Getting entity title else empty list
+total_entities = soup.find('div',{'class':'entity-results'})
 
+if total_entities==None:
+    entity_titles = []
+else:
+    entity_titles = []
+    for i in total_entities:
+        if soup.find('div',{'class':'entity-results','class':'entity-title'})==None:
+            entity_titles = []
+        else:
+            result = soup.find('div',{'class':'entity-results','class':'entity-title'}).text
+            if result not in entity_titles:
+                entity_titles.append(result)
+print(entity_titles)
 
-while True:
-    query = input('Query :: ')
-    if query=='exit' or query=='break' or query=='quit':
-        break
-    entity_results = entity_function(query)
-    print()
-    if entity_results:
-        for i in entity_results:
-            if i:
-                print(i)
-        print()
-        time.sleep(3)
-        web_function(query)
-    elif entity_results==False:
-        time.sleep(3)
-        web_function(query)
+# Getting entity type else empty list
+if total_entities==None:
+    entity_types = []
+else:
+    entity_types = []
+    for i in total_entities:
+        if soup.find('div',{'class':'entity-results','class':'entity-type'})==None:
+            entity_types = []
+        else:
+            result = soup.find('div',{'class':'entity-results','class':'entity-type'}).text
+            if result not in entity_types:
+                entity_types.append(result)
+print(entity_types)
+
+# Getting entity description else empty list
+if total_entities==None:
+    entity_description = []
+else:
+    entity_description = []
+    for i in total_entities:
+        if soup.find('div',{'class':'entity-results','class':'entity-description'})==None:
+            entity_description = [] 
+        else:
+            result = soup.find('div',{'class':'entity-results','class':'entity-description'}).text
+            try:    result = result.replace('\n','')
+            except: pass
+            try:    result = result.replace('Read more','')
+            except: pass
+            result = result.strip()
+            if result not in entity_description:
+                entity_description.append(result)
+print(entity_description)
+
+# Getting url else emtpy list
+if soup.find('div',{'class':'entity-section','class':'entity-url'})==None:
+    entity_url = []
+else:
+    total_entities = soup.find('div',{'class':'entity-section','class':'entity-url'}).find_all('a')
+    entity_url = []
+    for i in total_entities:
+        result = i.get('href')
+        if result not in entity_url:
+            entity_url.append(result)
+print(entity_url)
+
+# Getting wikipedia link
+if soup.find('div',{'class':'entity-content','class':'entity-contract'})==None:
+    entity_wikipedia = []
+else:
+    total_entities = soup.find('div',{'class':'entity-content','class':'entity-contract'}).find_all('a')
+    entity_wikipedia = []
+    for i in total_entities:
+        result = i.get('href')
+        if result not in entity_wikipedia:
+            entity_wikipedia.append(result)
+print(entity_wikipedia)
+
+## Third Part Loading webpages

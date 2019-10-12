275faf93b809b4480deb050a174dfce200535914
Sahil Uppal <sahil2kuppal@gmail.com>
2019-10-12 19:17:44 +0530

    cyan added

--- search.py
+++ search.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from color import Color
+import os
 
 query = input("Query:: ")
 parser = 'lxml'
@@ -24,7 +26,6 @@ else:
     except:
         pass
     spell_title = spell_title.strip()
-print(spell_title)
 
 # Getting spell description
 if soup.find('div',{'class':'web-results','class':'query-spell'}) == None:
@@ -36,7 +37,6 @@ else:
     except:
         pass
     spell_description = spell_description.strip()
-print(spell_description)
 
 ## Second part loading the entity
 # Getting entity title else empty list
@@ -53,7 +53,6 @@ else:
             result = soup.find('div',{'class':'entity-results','class':'entity-title'}).text
             if result not in entity_titles:
                 entity_titles.append(result)
-print(entity_titles)
 
 # Getting entity type else empty list
 if total_entities==None:
@@ -67,7 +66,6 @@ else:
             result = soup.find('div',{'class':'entity-results','class':'entity-type'}).text
             if result not in entity_types:
                 entity_types.append(result)
-print(entity_types)
 
 # Getting entity description else empty list
 if total_entities==None:
@@ -86,7 +84,6 @@ else:
             result = result.strip()
             if result not in entity_description:
                 entity_description.append(result)
-print(entity_description)
 
 # Getting url else emtpy list
 if soup.find('div',{'class':'entity-section','class':'entity-url'})==None:
@@ -98,7 +95,6 @@ else:
         result = i.get('href')
         if result not in entity_url:
             entity_url.append(result)
-print(entity_url)
 
 # Getting wikipedia link
 if soup.find('div',{'class':'entity-content','class':'entity-contract'})==None:
@@ -110,6 +106,65 @@ else:
         result = i.get('href')
         if result not in entity_wikipedia:
             entity_wikipedia.append(result)
-print(entity_wikipedia)
 
 ## Third Part Loading webpages
+# Getting site titles else emtpy list
+total_results = soup.find('div',{'class':'web-results'})
+if total_results==None:
+    site_titles = []
+else:
+    total_results = soup.find('div',{'class':'web-results'}).find_all('div',{'class':'site-result'})
+    site_titles = []
+    for i in total_results:
+        result = i.find('div',{'class':'site-title'}).text
+        site_titles.append(result)
+
+# Getting site urls else empty list
+if total_results == None:
+    site_urls = []
+else:
+    total_results = soup.find('div',{'class':'web-results'}).find_all('div',{'class':'site-result'})
+    site_urls = []
+    for i in total_results:
+        result = i.find('div',{'class':'site-url'}).find('a')
+        site_urls.append(result.get('href'))
+
+# Getting site descriptions else empty list
+if total_results == None:
+    site_descriptions = []
+else:
+    total_results = soup.find('div',{'class':'web-results'}).find_all('div',{'class':'site-result'})
+    site_descriptions = []
+    for i in total_results:
+        result = i.find('div',{'class':'site-description'}).text
+        site_descriptions.append(result)
+
+
+# Third Part Stacking all of these results in a beautiful way
+# site title
+# site url
+# site description
+
+color = Color()
+os.system('clear')
+
+if spell_title:
+    print()
+    print(color.GREEN + spell_title + color.END)
+    print(color.RED + spell_description + color.END)
+
+if entity_titles:
+    print()
+    print("Title  ::   ",entity_titles[0])
+    print("Type   ::   ",entity_types[0] if entity_types else "Unknown")
+    print('\nDescription :: '+color.YELLOW +entity_description[0] + color.END+'\n' if entity_description else "Unknown")
+    print("URL       :: ",entity_url[0] if entity_url else "Unknown")
+    print("Wikipedia :: ",entity_wikipedia[0]+'\n' if entity_wikipedia else "Unknown\n")
+
+if site_titles:
+    print()
+    for title,url,description in zip(site_titles,site_urls,site_descriptions):
+        print(color.BOLD + title + color.END)
+        print(color.CYAN + url + color.END)
+        print(description) 
+        print()
